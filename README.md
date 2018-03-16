ff44e0c6939d1f31b2d888741ffc8ec2f3dbf8a9
betachen <betachen@yeah.net>
2018-03-17 05:59:50 +0800

    mvs-gambling add

---
+++ README.md
@@ -0,0 +1,65 @@
+
+基于区块链的竞猜程序Demo-元界
+----------------
+
+### 背景
+实际上基于区块链的竞猜类游戏早就诞生了，最早有在比特币上竞猜区块HASH的游戏。
+参考了类似思路，我也做了一个。
+不过不同的是，我在设计MVS(元界)的接口试弄了很多顺手用的接口，正好在这里用上了。
+所以这个程序最大的特色是不需要任何服务器。
+你所要做的就是打开mvs-gambling.py 前面几行，填写好账户名密码，以及地址即可。
+让钱包和mvs-gambling.py同时运行即可。
+
+从逻辑上来看，这更像是一个自助结算的中心化小程序，而不是DAPP。
+这里可以扩展到有庄模式的gambling游戏。
+
+运行：
+```
+$ wget http://sfo.newmetaverse.org/mvs-download/mvs-linux-x86_64-v0.7.5.tar.gz
+$ tar -xzvf mvs-linux-x86_64-v0.7.5.tar.gz
+$ cd mvs-linux-x86_64-v0.7.5
+$ ./mvsd -d
+$ ./mvs-cli getnewaccount chenhao chenhao
+$ ./mvs-cli getnewaddress chenhao chenhao
+$ #配置mvs-gambling.py
+$ ./mvs-gambling.py
+```
+最后往里面的地址打币即可，开出的区块会按照押注进行判断，自动结算，包含赔率计算。
+
+### 游戏玩法
+
+先投注：
+
+是-请押注到地址A: [**MU93d3PEiVNUhjynXYoTDZK1j6CJRxSLgx**](http://mymvs.info/address/MU93d3PEiVNUhjynXYoTDZK1j6CJRxSLgx)
+否-请押注到地址B: [**MRQr3uG6BUup2TDknuwL8U6ERnzQsNDFaF**](http://mymvs.info/address/MRQr3uG6BUup2TDknuwL8U6ERnzQsNDFaF)
+
+<!-- more -->
+
+### 竞猜下一个10的整数倍高度的区块哈希的最后一位是否是数字?
+
+是-请押注到地址A: [**MU93d3PEiVNUhjynXYoTDZK1j6CJRxSLgx**](http://mymvs.info/address/MU93d3PEiVNUhjynXYoTDZK1j6CJRxSLgx)
+否-请押注到地址B: [**MRQr3uG6BUup2TDknuwL8U6ERnzQsNDFaF**](http://mymvs.info/address/MRQr3uG6BUup2TDknuwL8U6ERnzQsNDFaF)
+
+例如:
+区块高度958210, 958220, 958230, 区块哈希分别是
+```
+82450071f44e94be72779b3cbb8ff6917de5e8c67df572cafcc8e9ea4359f67d
+63fc451a02215318181343770d80e15a04c5d838741b20dc9fc331550a6d066e
+2afbb7269f666c50d6e37ff16df3bf37cf4bd64f0d33787d2d854287f2987226
+```
+那么开奖结果分别是: 否，否，是
+
+## 中奖概率
+区块哈希的组成其实是十六进制，所以押注的概率:
+是 62.5% - 否 37.5%
+
+## 赔率计算公式
+A胜：  (A+B) * 0.98 / A
+B胜：  (A+B) * 0.98 / B
+在[元界的区块浏览器](http://mymvs.info)上可查询上述地址的余额，即可计算赔率。
+
+## 开奖与结算
+每次开奖后3个区块开始发起上一轮结算, MVS上的区块时间约30秒，所以一轮大约5~7分钟，奖金结算控制在3分钟以内。
+系统抽取佣金2%。
+
+如果本轮有任意一方押注资金为0，则无法开奖，所有奖金注入下一轮进行开奖。
---
+++ mvs-gambling.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import requests
+import json
+import time
+import datetime
+import logging
+
+ROUND_BLOCK_UNIT = 10
+SETTLEMENT_BLOCK = 3
+
+################################################################
+class Command:
+    name="chenhao"
+    passwd="ChenHao2009#"
+    #bronze
+    address_A="MU93d3PEiVNUhjynXYoTDZK1j6CJRxSLgx"
+    address_B="MRQr3uG6BUup2TDknuwL8U6ERnzQsNDFaF"
+    mychange="MHv1JGzbhepj3FoDGG169J6SDsY86z4CYa"
+
+    def __init__(self, url):
+        self.url = url
+
+    def getinfo(self):
+        cmd = {"jsonrpc":"2.0","method":"getinfo","params":[],"id":25}
+        ret = requests.post(self.url, json.dumps(cmd))
+        return json.loads(ret.content)
+
+    def getblockheaderhash(self, round_height):
+        cmd = {"jsonrpc":"2.0","method":"getblockheader","params":[{"height":round_height}],"id":25}
+        ret = requests.post(self.url, json.dumps(cmd))
+        return json.loads(ret.content)["result"]["hash"]
+
+    def getbalance(self):
+        cmd = {"jsonrpc":"2.0","method":"getbalance","params":[self.name, self.passwd],"id":25}
+        ret = requests.post(self.url, json.dumps(cmd))
+        return json.loads(ret.content)
+
+    def listtxs_A(self, height_range):
+        """获取A押注方-数字 充值队列- 地址指定作为参数貌似无效"""
+        cmd = {"jsonrpc":"2.0","method":"listtxs","params":[self.name,self.passwd,{"height":height_range,"address":self.address_A}],"id":25}
+        ret = requests.post(self.url, json.dumps(cmd))
+        return json.loads(ret.content)
+
+    def listtxs_B(self, height_range):
+        """获取B押注方-字母 充值队列- 地址指定作为参数貌似无效"""
+        cmd = {"jsonrpc":"2.0","method":"listtxs","params":[self.name,self.passwd,{"height":height_range,"address":self.address_B}],"id":25}
+        ret = requests.post(self.url, json.dumps(cmd))
+        return json.loads(ret.content)
+
+    def sendmore(self, receipt):
+        cmd = {"jsonrpc":"2.0","method":"sendmore","params":[self.name,self.passwd,{"receivers":receipt,"mychange":self.mychange}],"id":25}
+        ret = requests.post(self.url, json.dumps(cmd))
+        return json.loads(ret.content)
+
+    def collcetion(self, amount):
+        receipt = self.mychange + ":" + str(amount)
+        self.sendmore(receipt)
+
+    def get_address_etp(self):
+        cmd = {"jsonrpc":"2.0","method":"getaddressetp","params":[self.address_A],"id":25}
+        ret = requests.post(self.url, json.dumps(cmd))
+        a = json.loads(ret.content)["result"]["balance"]["unspent"]
+
+        cmd = {"jsonrpc":"2.0","method":"getaddressetp","params":[self.address_B],"id":25}
+        ret = requests.post(self.url, json.dumps(cmd))
+        b = json.loads(ret.content)["result"]["balance"]["unspent"]
+        return (a,b)
+
+################################################################
+def get_send_queue(cmd, height_range, queue_type):
+    """获取两个队列的充值总额，用于计算赔率"""
+    json_txs = None
+    if queue_type == 'A':
+        json_txs = cmd.listtxs_A(height_range);
+        address = cmd.address_A
+    if queue_type == 'B':
+        json_txs = cmd.listtxs_B(height_range);
+        address = cmd.address_B
+
+    try:
+        if json_txs["error"]["code"] ==  2003:
+            #logging.critical( "池子%s在本轮没有任何充值进来,本轮放弃" % queue_type)
+            return 0
+    except:
+        logging.critical( "池子%s有充值进来" % queue_type)
+
+    txs = json_txs["result"]["transactions"]
+    recevie_sum = 0
+    for tx in txs:
+        if tx["direction"] == "receive":
+            for output in tx["outputs"]:
+                if output["own"] == True and output["locked_height_range"] == 0 and output["address"] == address:
+                    recevie_sum += output["etp-value"]
+                    logging.critical( "(%s, %s, %s, %s, %s)" % (tx["hash"], height_range, str(tx["inputs"][0]["address"]), output["etp-value"], queue_type))
+    return recevie_sum
+
+################################################################
+def payment(cmd, queue_type, last_round, odd, mychange):
+    logging.critical( "--- round(%s)支付开始 --- " % last_round)
+    if queue_type == 'A':
+        json_txs = cmd.listtxs_A(last_round);
+        address = cmd.address_A
+    if queue_type == 'B':
+        json_txs = cmd.listtxs_B(last_round);
+        address = cmd.address_B
+
+    txs = json_txs["result"]["transactions"]
+    for tx in txs:
+        if tx["direction"] == "receive":
+            for output in tx["outputs"]:
+                if output["own"] == True and output["locked_height_range"] == 0 and output["address"] == address:
+                    pay = int(output["etp-value"] * odd)
+                    receipt = str(tx["inputs"][0]["address"]) + ":" + str(pay)
+                    # WARNING - Payment here
+                    logging.critical( "支付给 %s" % receipt)
+                    tx = cmd.sendmore(receipt)
+                    try:
+                        logging.critical( "支付结果: %s" % str(tx["result"]["transaction"]["hash"]))
+                    except:
+                        logging.critical( "支付错误: %s" % str(tx["error"]))
+    cmd.sendmore(mychange)
+    logging.critical( "--- round(%s)支付结束 --- " % last_round)
+
+################################################################
+
+def settlement(cmd, round_height, round_block):
+    logging.critical( "========  %s - %s 轮清算回合开始 =========" % (round_height - round_block, round_height))
+    last_round = str(round_height - round_block) + ":" + str(round_height + 1)
+    blockhash = cmd.getblockheaderhash(round_height)
+
+    sum_A = get_send_queue(cmd, last_round, 'A')
+    sum_B = get_send_queue(cmd, last_round, 'B')
+
+    balances = cmd.get_address_etp()
+
+    if sum_A == 0 and sum_B == 0:
+        logging.critical( "双方池子在本轮没有任何充值,本轮放弃清算")
+    elif sum_A == 0 or sum_B == 0:
+        logging.critical( "有一方池子在本轮没有任何充值,资金直接进入下一轮")
+        logging.critical( "========  %s - %s 轮清算回合结束 =========" % (round_height - round_block, round_height))
+        round_block += ROUND_BLOCK_UNIT
+    else:
+        # 实施清算则将round_block重置为1单位的UNIT
+        round_block = ROUND_BLOCK_UNIT
+        # 计算赔率和支付方向,抽水2%
+        payto = 'B'
+        odd = (sum_A + sum_B) * 0.98 / float(sum_B)
+        logging.critical( "blockhash: %s" % blockhash)
+        #if blockhash[-1].isdigit() and blockhash[-1] != '9' and blockhash[-1] != '8':
+        if blockhash[-1].isdigit():
+            payto = 'A'
+            odd = (sum_A + sum_B) * 0.98 / float(sum_A)
+
+        #抽水
+        mychange = int((sum_A + sum_B) * 0.02)
+
+        logging.critical( "本轮实施清算,A: 余额[%s]-sum_A[%s], B: 余额[%s]-Sum_B[%s]" % (balances[0], sum_A, balances[1], sum_B))
+        logging.critical( "本轮 %s 胜, 最终赔率 %s" % (payto, odd))
+        payment(cmd, payto, last_round, odd, mychange)
+        logging.critical( "========  %s - %s 轮清算回合结束 =========" % (round_height - round_block, round_height))
+
+    return round_block
+
+################################################################
+
+def main(cmd):
+    """
+    Get entry
+    """
+    logging.critical( ">>>>>>>>> MVS 区块大竞猜 启动 <<<<<<<<<")
+
+    # 每轮应该扫描充值队列的长度
+    round_block = ROUND_BLOCK_UNIT
+
+    # 当前回合结束的高度
+    round_height = 0
+    # 产生新块的判断
+    is_new_round = False
+    tmp_height = cmd.getinfo()["result"]["height"]
+
+    while True:
+        """
+        每3秒check一次
+        """
+        time.sleep(3)
+
+        """
+        每round_block块开奖一次，即块号后两位是00，开奖后SETTLEMENT_BLOCK个块内结算完毕
+        清算的时机不能晚于下一次开奖
+        """
+        info = cmd.getinfo()
+        last_height = info["result"]["height"]
+        if last_height == tmp_height:
+            continue
+        else:
+            # 产生了新块
+            if last_height - tmp_height > 1:
+                logging.critical( ">>> WARNING 3秒扫描%s:%s漏块了" % (tmp_height, last_height))
+                last_height = tmp_height + 1
+                tmp_height += 1
+                logging.critical( ">>> WARNING 扫描高度自动调整为 %s" % last_height)
+            else:
+                tmp_height = last_height
+
+        if is_new_round == False and last_height % ROUND_BLOCK_UNIT == 0:
+            is_new_round = True
+            round_height = last_height
+            logging.critical( "################### 新回合 %s - %s 开始 ###################" % (round_height, round_height + ROUND_BLOCK_UNIT))
+        else:
+            logging.critical( "%s: 当前回合 %s - %s 等待用户押注" % (last_height, round_height, round_height + ROUND_BLOCK_UNIT))
+
+        # 发起清算的时机：延迟round_block个区块，校验hash
+        if is_new_round and last_height - round_height >= SETTLEMENT_BLOCK:
+            # settlement go
+            round_block = settlement(cmd, round_height, round_block)
+            logging.critical( "round_block %s" % round_block)
+            # reset
+            is_new_round = False
+
+################################################################
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.CRITICAL,
+                format='%(asctime)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='mvs-gambling.log',
+                filemode='a')
+
+    cmd = Command("http://127.0.0.1:8820/rpc/v2")
+    main(cmd);
